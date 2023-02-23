d54a288da511eee03509068ccd77216c7bad12d7
DanielleColucci <ddcolucci99@gmail.com>
2023-02-23 12:00:32 -0500

    create Sample model

---
+++ migrations/20230223165210-create-sample.js
@@ -0,0 +1,55 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Samples', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      MRN: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      sampleNumber: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      date: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cellLine: {
+        type: Sequelize.ENUM('In Process', 'Finished', 'Failed', 'NA'),
+        defaultValue: 'N/A',
+        allowNull: false
+      },
+      PDXModel: {
+        type: Sequelize.ENUM('In Process', 'Finished', 'Failed', 'NA'),
+        defaultValue: 'N/A',
+        allowNull: false
+      },
+      profileId: {
+        type: Sequelize.INTEGER,
+        allowNull: false, 
+        references: {
+          model: 'Profiles',
+          key: 'id'
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Samples');
+  }
+};
---
+++ models/sample.js
@@ -0,0 +1,52 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Sample extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Sample.init({
+    MRN: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    sampleNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    cellLine: {
+      type: DataTypes.ENUM('In Process', 'Finished', 'Failed', 'N/A'),
+      defaultValue: 'N/A',
+      allowNull: false,
+    },
+    PDXModel: {
+      type: DataTypes.ENUM('In Process', 'Finished', 'Failed', 'NA'),
+      defaultValue: 'N/A',
+      allowNull: false
+    },
+    profileId: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      references: {
+        model: 'Profiles',
+        key: 'id',
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'Sample',
+  });
+  return Sample;
+};

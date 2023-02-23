52f2f6cdb2ed503eb2ca9f82432a468af9a04858
DanielleColucci <ddcolucci99@gmail.com>
2023-02-23 13:43:32 -0500

    define route and controller function to create a sample

--- controllers/samples.js
+++ controllers/samples.js
@@ -1,6 +1,16 @@
 const { Sample } = require('../models')
 
+async function create(req, res) {
+  try {
+    req.body.profileId = req.user.profile.id
+    const sample = await Sample.create(req.body)
+    res.status(200).json(sample)
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ err: error })
+  }
+}
 
 module.exports = {
-  
+  create,
 }
